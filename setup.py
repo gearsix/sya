70a4687e0cdc50768a30ebb2f00f593c7a17866a
gearsix <gearsix@tuta.io>
2022-12-01 21:21:39 +0000

    updated setup.py details

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ import setuptools
 DESC='download & split long youtube videos as audio tracks'
 LONG_DESC='download long youtube videos as audio tracks using youtube-dl and split them into multiple audio tracks using ffmpeg.'
 
-with open('README.txt', 'r') as f:
+with open('README.md', 'r') as f:
     LONG_DESC = f.read()
 
 setuptools.setup(
 	name='sya',
-	version='0.8.0',
+	version='1.0.1',
 	author='gearsix',
 	author_email='gearsix@tuta.io',
 	description=DESC,
         long_description=LONG_DESC,
-        long_description_content_type='text/plain',
+        long_description_content_type='text/markdown',
 	url='https://notabug.org/gearsix/sya',
 	packages=setuptools.find_packages(),
 	classifiers=[
